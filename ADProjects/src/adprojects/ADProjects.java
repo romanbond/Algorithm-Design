2e7bc06d3acf00a59a52e5dcd10b28e8ad15c2aa
Roman Bond <romanjbond@gmail.com>
2016-09-02 13:22:27 -0400

    Modded AD file

--- ADProjects/src/adprojects/ADProjects.java
+++ ADProjects/src/adprojects/ADProjects.java
@@ -1,35 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package adprojects;
 
-/**
- *
- * @author kepler
- */
 public class ADProjects {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         long startTime = System.currentTimeMillis();
-        
         int[] A = new int[1000000];
         int sum = 0;
-        
         for (int i = 0; i < A.length; i++){
             sum = sum + A[i];
         }
         long stopTime = System.currentTimeMillis();
-        
         long elapsedTime = stopTime - startTime;
-        
+
         System.out.println(elapsedTime);
-        
     }
-    
+
 }

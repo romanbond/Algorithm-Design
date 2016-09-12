e995ff646c609f701fd811205bf970def283b972
Roman Bond <romanjbond@gmail.com>
2016-09-12 07:54:50 -0400

    Changed Methods

--- ADProjects/src/adprojects/ADProjects.java
+++ ADProjects/src/adprojects/ADProjects.java
@@ -6,7 +6,8 @@ package adprojects;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+//import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,9 +15,93 @@ import static java.lang.Integer.parseInt;
 import java.util.Scanner;
 
 public class ADProjects {
-
+    
+    
     public static void main(String[] args) throws IOException {
+        //First we create the file with nothing in it
+        //createFile();
+        //Next we add literally "something" to the file
+        //purgeFile();
+        //addToFile("s");
+        for(int i=0; i<0;i++){
+            String s = display() + Integer.toString(i);
+            addToFile(s);
+        }
+        System.out.println(display());
+        purgeFile();
+    }
+    public static void getNumber() throws IOException{
+        String output = "";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number or type clear");
+        String userInput = in.nextLine();
+        if(in.nextLine().equals("clear")){
+            purgeFile();
+        }else{
+        output = in.nextLine() + stacking();
+        addToFile(output);
+        }
+    }
+    public static void createFile() throws IOException{
+        addToFile("");
+    }
+    public static void addToFile(String addedVar) throws IOException{
+        String text = addedVar;
+        BufferedWriter output = null;
+        try {
+            File file = new File("example.txt");
+            output = new BufferedWriter(new FileWriter(file));
+            output.write(text);
+        } catch ( IOException e ) {
+        } finally {
+            if ( output != null ) {
+                output.close();
+            }
+        }
+    }
+    public static void purgeFile() throws IOException{
+        addToFile("");
         /*
+        String text = "Purged";
+        BufferedWriter output = null;
+        try {
+            File file = new File("example.txt");
+            output = new BufferedWriter(new FileWriter(file));
+            output.write(text);
+        } catch ( IOException e ) {
+        } finally {
+            if ( output != null ) {
+                output.close();
+            }
+        }*/
+    }
+    public static void calculation(){
+        
+    }
+    public static String stacking() throws IOException{
+        Scanner in = new Scanner(new FileReader("example.txt"));
+        //convert the scanned material to a string variable
+        String existingVars = in.next();
+        //addToFile("");
+        return existingVars;
+    }
+    public static String display() throws IOException{
+        Scanner in = new Scanner(new FileReader("example.txt"));
+        //convert the scanned material to a string variable
+        String existingVars = in.next();
+        //addToFile("");
+        //System.out.println(existingVars);
+        return existingVars;
+    }
+    
+    
+    
+    
+
+    public static void OldMain () throws IOException {
+        /*
+        //Runtime example from class
+
         long startTime = System.currentTimeMillis();
         int[] A = new int[1000000];
         int sum = 0;
@@ -27,11 +112,11 @@ public class ADProjects {
         long elapsedTime = stopTime - startTime;
 
         System.out.println(elapsedTime);
-        */ 
-        
-        
+        */
+
+
         // This text is in the text file and is overwritten everytime it runs
-        String text = Test();//this grabs input from the test method below
+        String text = inputSection();//this grabs input from the test method below
         //String text2 = "1";
         BufferedWriter output = null;
         try {
@@ -45,38 +130,48 @@ public class ADProjects {
             output.close();
           }
         }
-        
+        //scanning();
+    }
+
+    public static String inputSection() throws IOException{
+        String text = "";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number or type clear");
+        if(in.nextLine().equals("clear")){
+            purgeText();
+        }else{
+        text = in.nextLine() + scanning();
+        }
+        System.out.println("This is the text test  "+ text);
+        return text;
+    }
+
+    public static String scanning() throws IOException {
         //This is where we read the text file
         Scanner in = new Scanner(new FileReader("example.txt"));
         //convert the scanned material to a string variable
         String out = in.next();
         System.out.println(out);
         
-        //text values converted to a double and subtracted
-        double outNum = parseInt(out);
-        System.out.println(outNum-2);
-        
-        //Here we compare the output from the textfile
-        if(out.equals("12")){
-            System.out.println("Truth");
-        }else{
-            System.out.println("False");
-        }
-        
-        
-        //Test();
+        return out;
     }
 
-    public static String Test (){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter a number:");
-        String text = in.nextLine();
-        
-        System.out.println(text);
-        //String text = "11";
-        
-        return text;
-        
+    public static void purgeText () throws IOException{
+        // This text is in the text file and is overwritten everytime it runs
+        String text = "";//this grabs input from the test method below
+        //String text2 = "1";
+        BufferedWriter output = null;
+        try {
+            File file = new File("example.txt");
+            output = new BufferedWriter(new FileWriter(file));
+            output.write(text);
+            //output.write(text2);
+        } catch ( IOException e ) {
+        } finally {
+          if ( output != null ) {
+            output.close();
+          }
+        }
     }
 
 }
